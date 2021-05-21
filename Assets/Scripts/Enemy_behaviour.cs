5a5b0192ff720f260956747315677fb67aa43113
Gonzaaa <gonzaloantuna@hotmail.com>
2021-05-21 04:35:01 -0300

    Programación Enemigo

--- Assets/Scripts/Enemy_behaviour.cs
+++ Assets/Scripts/Enemy_behaviour.cs
@@ -4,15 +4,141 @@ using UnityEngine;
 
 public class Enemy_behaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    #region Public Variables 
+    public Transform rayCast;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+    public float attackDistance; // Distancia minima para que el enemigo me ataque
+    public float moveSpeed;
+    public float timer; //Timer para el cooldown entre ataques 
+    #endregion
+
+    #region Private Variables
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator anim;
+    private float distance;
+    private bool attackMode;
+    private bool inRange;
+    private bool cooling;
+    private float intTimer;
+    #endregion
+
+    void Awake()
     {
-        
+        intTimer = timer; //Guarda el valor incial del tiempo
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inRange)
+        {
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
+            RaycastDebugger();
+        }
+
+        //Cuando el jugador es detectado
+        if(hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        else if(hit.collider == null)
+        {
+            inRange = false;
+        }
+
+        if(inRange == false)
+        {
+            anim.SetBool("canWalk", false);
+            StopAttack();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Player")
+        {
+            target = trig.gameObject;
+            inRange = true;
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if(distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+        else if(attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+
+        if(cooling)
+        {
+            Cooldown();
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    void Move()
+    {
+        anim.SetBool("canWalk", true);
+        if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Enemy_Attack"))
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void Attack()
+    {
+        timer = intTimer; //Resetea el Timer cuando el jugador entra en el rango de ataque
+        attackMode = true; //Chequea que el enemigo pueda atacar o no
+
+        anim.SetBool("canWalk", false);
+        anim.SetBool("Attack", true);
+    }
+
+    void Cooldown()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0 && cooling && attackMode)
+        {
+            cooling = true;
+            timer = intTimer;
+        }
+    }
+
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+        anim.SetBool("Attack", false);
+    }
+
+    void RaycastDebugger()
+    { 
+        if(distance > attackDistance)
+            {
+                Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+        }   
+        else if(attackDistance > distance)
+        {
+                Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+        }
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
     }
 }
